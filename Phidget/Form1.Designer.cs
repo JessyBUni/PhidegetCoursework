8932b1b59898a9bd6c85ba4a1ed9279b9d472908
JessyBUni <47369178+JessyBUni@users.noreply.github.com>
2019-08-08 18:04:04 +0100

    Add files via upload

---
+++ Phidget/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Phidget
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TXT_WEIGH = new System.Windows.Forms.TextBox();
+            this.TXT_BANK = new System.Windows.Forms.TextBox();
+            this.BTN_BANK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TXT_WEIGH
+            // 
+            this.TXT_WEIGH.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_WEIGH.Location = new System.Drawing.Point(17, 59);
+            this.TXT_WEIGH.Name = "TXT_WEIGH";
+            this.TXT_WEIGH.Size = new System.Drawing.Size(180, 38);
+            this.TXT_WEIGH.TabIndex = 1;
+            this.TXT_WEIGH.Text = "£0.00";
+            this.TXT_WEIGH.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // TXT_BANK
+            // 
+            this.TXT_BANK.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_BANK.Location = new System.Drawing.Point(395, 59);
+            this.TXT_BANK.Name = "TXT_BANK";
+            this.TXT_BANK.Size = new System.Drawing.Size(180, 38);
+            this.TXT_BANK.TabIndex = 2;
+            this.TXT_BANK.Text = "£0.00";
+            this.TXT_BANK.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // BTN_BANK
+            // 
+            this.BTN_BANK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_BANK.Location = new System.Drawing.Point(213, 59);
+            this.BTN_BANK.Name = "BTN_BANK";
+            this.BTN_BANK.Size = new System.Drawing.Size(130, 37);
+            this.BTN_BANK.TabIndex = 3;
+            this.BTN_BANK.Text = "Bank coins";
+            this.BTN_BANK.UseVisualStyleBackColor = true;
+            this.BTN_BANK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(349, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Weighed coin value :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(390, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Banked value :";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(17, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Tea";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Products Available : ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(17, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 37);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Coffee";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(17, 241);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 37);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Hot Chocolate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(208, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "£1.00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(208, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "£2.00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(208, 247);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "£2.00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(503, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 25);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "£4.00";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(503, 204);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 25);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "£3.00";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(503, 161);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "£1.00";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(312, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 37);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Sandwich";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(312, 198);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 37);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Biscuits";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(312, 155);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(144, 37);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Crisps";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 303);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTN_BANK);
+            this.Controls.Add(this.TXT_BANK);
+            this.Controls.Add(this.TXT_WEIGH);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TXT_WEIGH;
+        private System.Windows.Forms.TextBox TXT_BANK;
+        private System.Windows.Forms.Button BTN_BANK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Phidget/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Phidget22;
+using Phidget22.Events;
+//using Phidget22.ExampleUtils;
+
+namespace Phidget
+{
+    
+    public partial class Form1 : Form
+    {
+        VoltageInput vin = new VoltageInput();
+        int bank = 0;
+        public void vin_InputChange(object sender, VoltageInputSensorChangeEventArgs e)
+        {
+            //this.textBox1.Text = e.SensorValue.ToString();
+        }
+        public void openPhidget()
+        {
+            vin.DeviceSerialNumber = 315981; //Address
+            
+            //vin.HubPort = 2;
+            vin.Channel = 0;
+            vin.SensorChange += new VoltageInputSensorChangeEventHandler (vin_InputChange);
+
+            vin.Open(5000); //Open
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            openPhidget();
+
+            //vin.VoltageChange += voltageChange;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vin.VoltageChange += vin_VoltageChange;
+        }
+        void vin_VoltageChange(object sender, Phidget22.Events.VoltageInputVoltageChangeEventArgs e)
+        {
+            //textBox1.Text = vin.Voltage.ToString() + " V";
+            int value = (int)(vin.Voltage / 0.3);
+            TXT_WEIGH.Text = "£"+value.ToString()+".00";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int price = 2;
+            getBankValue(out int m, out bool pass);
+            if (pass && m >= price)
+            {
+                setBank(-price);
+            }
+            else { message(); }
+            setbuttons();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int price = 3;
+            getBankValue(out int m, out bool pass);
+            if (pass && m >= price)
+            {
+                setBank(-price);
+            }
+            else { message(); }
+            setbuttons();
+        }
+        public void getBankValue(out int m, out bool pass)
+        {
+            string txt_money = TXT_BANK.Text;
+            string[] value = txt_money.Split('.');
+            string v = value[0];
+            v = v.TrimStart('£');
+            pass = Int32.TryParse(v, out int money);
+            m = money;
+        }
+        public void setBank(int money)
+        {
+            bank += money;
+            TXT_BANK.Text = "£" + bank + ".00";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt_money = TXT_WEIGH.Text;
+            string[] value = txt_money.Split('.');
+            string v = value[0];
+            v = v.TrimStart('£');
+            bool pass = Int32.TryParse(v, out int money);
+            if (pass)
+            {
+                setBank(money);
+            }
+            else { message(); }
+            setbuttons();
+        }
+        public void message()
+        {
+            MessageBox.Show("There is not enough money in the back to purchase this item","Insuficient funds",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int price = 1;
+            getBankValue(out int m, out bool pass);
+            if (pass && m >= price)
+            {
+                setBank(-price);
+            }
+            else { message(); }
+            setbuttons();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int price = 2;
+            getBankValue(out int m, out bool pass);
+            if (pass && m >= price)
+            {
+                setBank(-price);
+            }
+            else { message(); }
+            setbuttons();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int price = 1;
+            getBankValue(out int m, out bool pass);
+            if (pass && m >= price)
+            {
+                setBank(-price);
+            }
+            else { message(); }
+            setbuttons();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int price = 4;
+            getBankValue(out int m, out bool pass);
+            if (pass && m >= price)
+            {
+                setBank(-price);
+            }
+            else { message(); }
+            setbuttons();
+        }
+        public void setbuttons()
+        {
+            if (bank < 1){button2.Enabled = false;}
+            else { button2.Enabled = true; }
+            if (bank < 2) { button3.Enabled = false; }
+            else { button3.Enabled = true; }
+            if (bank < 2) { button4.Enabled = false; }
+            else { button4.Enabled = true; }
+            if (bank < 1) { button7.Enabled = false; }
+            else { button7.Enabled = true; }
+            if (bank < 3) { button6.Enabled = false; }
+            else { button6.Enabled = true; }
+            if (bank < 4) { button5.Enabled = false; }
+            else { button5.Enabled = true; }
+
+        }
+    }
+    
+}
